64474f63299daa34f345fd30bb0bfb0834017901
Matthew Riley <mgriley97@gmail.com>
2017-06-17 15:22:34 -0400

    restructued project

---
+++ diy_dashboard/server.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html', name='hi')
--- server.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
