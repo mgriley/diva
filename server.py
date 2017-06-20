71b40dbe6233529a8b4a7c572943f02116e635ba
Matthew Riley <mgriley97@gmail.com>
2017-06-20 14:26:10 -0400

    switched to decorators for displays

--- server.py
+++ server.py
@@ -36,7 +36,6 @@ class FloatWidget(Widget):
     def parseForm(self, formData):
         return float(formData)
 
-report_generators = {}
 
 # given map of form data, return a map of inputs 
 def parse_widget_form_data(widgets, widgetFormData):
@@ -51,7 +50,7 @@ def parse_widget_form_data(widgets, widgetFormData):
             inputs[name] = widget.parseForm(formData)
     return inputs
 
-def register_report(name, figure_generator, widgets=[]):
+def get_report_generators(figure_generator, widgets=[]):
     # transform widgets to tuple array, of (argName, widget)
     # this gives each widget a unique key b/c arg names must be unique
     argNames = inspect.getargspec(figure_generator)[0]
@@ -76,76 +75,83 @@ def register_report(name, figure_generator, widgets=[]):
             widgetsHTML.append(widget.generateHTML(name))
         return widgetsHTML
 
-    report_generators[name] = {
-            'figure_generator': decorated_generator,
+    return  {'figure_generator': decorated_generator,
             'widgets_generator': widgets_generator}
+
+class Reporter:
+    def __init__(self):
+        self.report_generators = {}
+    
+    def display(self, name, widgets=[]):
+        def real_decorator(func):
+            generators = get_report_generators(func, widgets)
+            self.report_generators[name] = generators
+            return func
+        return real_decorator
+    
+    def get_reports(self):
+        reports = []
+        for key, value in self.report_generators.items():
+            reports.append({'name': key, 'href': key})
+        return reports
+
+reporter = Reporter()
 
 # generates an assertion error (unit test this later)
 # def figure_too_few():
     # return '<p>dksjalf</p>'
 # register_report('too_few', figure_too_few, [TextWidget('meh')])
 
+@reporter.display('too_many', [FloatWidget(6.5)])
 def figure_extra(a, b=6):
     return '<p>{} {}</p>'.format(a, b)
-register_report('too_many', figure_extra, [FloatWidget(6.5)])
 
-def figure_dec(a, b, c):
-    return '<p>{} {} {}</p>'.format(a, b, c)
-register_report('dec', figure_dec, [TextWidget('yo'), FloatWidget(2.5), TextWidget('ha')])
-
-# figure out how to do it like below. Decorators are made 
-# for this purpose!
-#@reports.add('report_name', [widget_list])
+@reporter.display('foo', [TextWidget('yo'), FloatWidget(20)])
 def figure_a(textName, floatName):
     print('inputs: {} {}'.format(textName, floatName))
     plt.figure()
     plt.plot([3,1,4,1,floatName], 'ks-', mec='w', mew=5, ms=20)
     htmlString = mpld3.fig_to_html(plt.gcf()) 
     return '<p>{}</p>{}'.format(textName, htmlString)
-register_report('foo', figure_a, [TextWidget("yo"), FloatWidget(20)])
 
-def figure_c():
-    df = pd.DataFrame(np.random.randn(20, 20))
-    tableString = df.to_string()
-    # return '<pre>' + tableString + '</pre>'
-    # TODO: set some basic formatting on the html table
-    return df.to_html()
-register_report('pan', figure_c)
+# def figure_c():
+    # df = pd.DataFrame(np.random.randn(20, 20))
+    # tableString = df.to_string()
+    # # return '<pre>' + tableString + '</pre>'
+    # # TODO: set some basic formatting on the html table
+    # return df.to_html()
+# register_report('pan', figure_c)
 
-def figure_d(textA, textB):
-    print(inputs)
-    return '<p>{} {}</p>'.format(textA, textB)
-register_report('yoo', figure_d, [TextWidget("aaaaaaaa"), TextWidget('bbbbbbb')])
+# def figure_d(textA, textB):
+    # print(inputs)
+    # return '<p>{} {}</p>'.format(textA, textB)
+# register_report('yoo', figure_d, [TextWidget("aaaaaaaa"), TextWidget('bbbbbbb')])
 
 # for i in range(100):
     # name = 'foo{}'.format(i)
 #     register_report(name, figure_a)
-
-reports = []
-for key, value in report_generators.items():
-    reports.append({'name': key, 'href': key})
 
 @app.route('/')
 @app.route('/<reportname>')
 def index(reportname=None):
     if reportname is None:
         return render_template(
-                'index.html', reports=reports)
-    figure_report = report_generators.get(reportname, None)
+                'index.html', reports=reporter.get_reports())
+    figure_report = reporter.report_generators.get(reportname, None)
     if figure_report is None:
         abort(404)
     figure_html = figure_report['figure_generator']()
     widgets_html = figure_report['widgets_generator']()
     return render_template(
             'figure_report.html',
-            reports=reports,
+            reports=reporter.get_reports(),
             figureHTML=figure_html,
             widgetsHTML=widgets_html)
 
 @app.route('/<reportname>', methods=['POST'])
 def updateFigure(reportname):
     print(request.get_json())
-    figure_report = report_generators.get(reportname, None)
+    figure_report = reporter.report_generators.get(reportname, None)
     if figure_report is None:
         abort(404)
     figure_generator = figure_report['figure_generator']
