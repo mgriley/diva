5437f4ff7a1434680183a39c7039ee7c69fd0d13
Matthew Riley <mgriley97@gmail.com>
2017-06-30 16:03:03 -0400

    started to work through the errors in the new approach, continue next time

--- diy_dashboard/static/script.js
+++ diy_dashboard/static/script.js
@@ -22,15 +22,17 @@ function changeTab(mouseEvent, reportId) {
 
 $(document).ready(function() {
     
+    console.log('setting up reports');
     // init all reports
     var reportElements = $('.report');
     for (var i = 0; i < reportElements.length; ++i) {
+        var reportElement = reportElements[i];
         var report = Reports.create();
         
         // setup the report's widgets
-        var widgetElements = reportElements[i].find('.user-widgets').children();
+        var widgetElements = $(reportElement).find('.user-widgets').children();
         for (var j = 0; j < widgetElements.length; ++j) {
-            var widgetType = widgetElements[j].data('widget-type');
+            var widgetType = $(widgetElements[j]).data('widget-type');
             var setupFunc = Reports.Widgets.setupMap[widgetType];
             var widget = setupFunc(widgetElements[0]);
             report.widgets.add(widget);
