5537f8a11870033d52258da15661e4981aa4bf32
Matthew Riley <mgriley97@gmail.com>
2017-08-03 10:49:09 -0400

    refactored Reporter reduce unecessary closure use, much simplified

--- diy_dashboard/reporter.py
+++ diy_dashboard/reporter.py
@@ -6,25 +6,57 @@ from .converters import convert_to_html
 from .widgets import parse_widget_form_data
 from flask import Flask, render_template, request, abort
 
-def get_report_generators(user_func, widgets=[]):
-    # transform widgets to tuple array, of (argName, widget)
-    # this gives each widget a unique key b/c arg names must be unique
-    argNames = inspect.getargspec(user_func)[0]
-    for index, widget in enumerate(widgets):
-        widgets[index] = (argNames[index], widget)
+# also TODO: there is no need for this crazy amount of closure,
+# why not just keep references to the figure and widgets, then
+# call these functions in the reporter class, thereby 
+# simplifying everything...
+# TODO: left off here. Figure out how to do validation.
+# Do the validation in parseForm, that is the easiest!
+# generate a json schema for the request object required to
+# update this report
+# def widgets_validator():
+        
+# widget_names = [w[0] for w in widgets]
+# properties = {}
+# for name, widget in widgets:
+    # properties[name] = {}
+# widgets_schema = {
+    # 'properties': properties
+    # 'required': widget_names,
+    # 'additionalProperties': False
+# }
+
+class Reporter:
+    def __init__(self):
+        self.reports = OrderedDict()
+    
+    def display(self, name, widgets=[]):
+        def real_decorator(user_func):
+            # transform widgets to tuple array, of (argName, widget)
+            # this gives each widget a unique key b/c arg names must be unique
+            argNames = inspect.getargspec(user_func)[0]
+            for index, widget in enumerate(widgets):
+                widgets[index] = (argNames[index], widget)
+
+            # save a ref to the user's func and widgets
+            self.reports[name] = {'user_func': user_func, 'widgets': widgets}
+
+            # the func is not modified
+            return user_func
+        return real_decorator
 
     # func that generates the figure by passing the user func the 
     # parsed form data, then converting the func's output to HTML
-    def figure_generator(widgetFormData={}):
-        inputs = parse_widget_form_data(widgets, widgetFormData)
-        output = user_func(**inputs)
+    def generate_figure_html(self, report, widgetFormData={}):
+        inputs = parse_widget_form_data(report['widgets'], widgetFormData)
+        output = report['user_func'](**inputs)
         return convert_to_html(output)
 
     # NB: widgets must also be generated b/c jinja render_templates
     # requires an app context to run
-    def widgets_generator():
+    def generate_widget_data(self, report):
         widget_data = []
-        for name, widget in widgets:
+        for name, widget in report['widgets']:
             # the widget type is the name of its python class
             # the JS setup func is Reports.Widgets.setupMap[widget_type]
             widget_type = type(widget).__name__
@@ -33,62 +65,16 @@ def get_report_generators(user_func, widgets=[]):
             data = {'name': name, 'type': widget_type, 'html': html}
             widget_data.append(data)
         return widget_data
-
-    # also TODO: there is no need for this crazy amount of closure,
-    # why not just keep references to the figure and widgets, then
-    # call these functions in the reporter class, thereby 
-    # simplifying everything...
-    # TODO: left off here. Figure out how to do validation.
-    # Do the validation in parseForm, that is the easiest!
-    # generate a json schema for the request object required to
-    # update this report
-    # def widgets_validator():
             
-    # widget_names = [w[0] for w in widgets]
-    # properties = {}
-    # for name, widget in widgets:
-        # properties[name] = {}
-    # widgets_schema = {
-        # 'properties': properties
-        # 'required': widget_names,
-        # 'additionalProperties': False
-    # }
-
-    return  {'figure_generator': figure_generator,
-            'widgets_generator': widgets_generator}
-
-class Reporter:
-    def __init__(self):
-        self.report_generators = OrderedDict()
-    
-    def display(self, name, widgets=[]):
-        def real_decorator(func):
-            # create the functions that generate a report for the 
-            # given func
-            generators = get_report_generators(func, widgets)
-            self.report_generators[name] = generators
-            # the func is not modified
-            return func
-        return real_decorator
-    
-    def get_reports(self):
-        reports = []
-        for key, value in self.report_generators.items():
-            widgets = value['widgets_generator']()
-            reports.append({'name': key, 'widgets': widgets})
-        return reports
-    
     def get_index(self):
+        report_data = []
+        for name, report in self.reports.items():
+            widgets = self.generate_widget_data(report)
+            report_data.append({'name': name, 'widgets': widgets})
         return render_template(
                 'index.html',
-                reports=self.get_reports())
-        
-    def html_for_figure(self, report_name, widget_values):
-        figure_report = self.report_generators.get(report_name, None)
-        figure_generator = figure_report['figure_generator']
-        reportHTML = figure_generator(widget_values)
-        return reportHTML
-    
+                reports=report_data)
+            
     def validate_request(self, json, base_schema):
         validate(json, base_schema)
         report_index = json['reportIndex']
@@ -116,7 +102,7 @@ class Reporter:
         return base_schema
 
     def run(self, host=None, port=None, debug=None, **options):
-        schema = self.create_schema()
+        # schema = self.create_schema()
 
         # setup the server
         app = Flask(__name__)
@@ -129,10 +115,11 @@ class Reporter:
         def update_figure():
             print(request.get_json())
             body = request.get_json()
-            self.validate_request(body, schema)
+            # self.validate_request(body, schema)
             report_index = body['reportIndex']
-            dict_items = list(self.report_generators.items())
+            dict_items = list(self.reports.items())
             report_name = dict_items[report_index][0]
-            return self.html_for_figure(report_name, body['widgetValues'])
+            report = self.reports.get(report_name, None)
+            return self.generate_figure_html(report, body['widgetValues'])
 
         app.run(host, port, debug, **options)
