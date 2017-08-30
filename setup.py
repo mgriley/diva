88f2153a888ab2de8e483cc9ccc5794f37f22fcf
Matthew Riley <mgriley97@gmail.com>
2017-08-30 11:06:28 -0400

    updated for next release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='diva',
-    version='0.1.4',
+    version='0.1.5',
     description='Analytics dashboards made simple',
     long_description='create a simple web analytics dashboard',
     url='https://github.com/mgriley/diva',
